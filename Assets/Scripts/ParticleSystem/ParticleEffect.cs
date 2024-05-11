b6637b81efe975b6c6d998be963ee81e119b5e26
Vlad Sunny <vladislavsunny@gmail.com>
2024-05-11 16:05:44 +0300

    HZ

--- Assets/Scripts/ParticleSystem/ParticleEffect.cs
+++ Assets/Scripts/ParticleSystem/ParticleEffect.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleEffect : MonoBehaviour
+namespace JJBA.VFX
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class ParticleEffect
     {
-        
-    }
+        // public ParticleSystem particleSystem;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/ParticleSystem/ParticleManager.cs
+++ Assets/Scripts/ParticleSystem/ParticleManager.cs
@@ -6,17 +6,7 @@ namespace JJBA.ParticleSystem
 {
     public class ParticleManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        // public ParticleEffect[] particleEffect;
     }
 }
 
