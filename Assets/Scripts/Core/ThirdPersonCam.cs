0e031c875a72e1de534fd7be574b6ca2e1deac41
Vladislav Sungurov <vladislavsunny@gmail.com>
2024-05-27 20:20:47 +0300

    Remove death camera

--- Assets/Scripts/Core/ThirdPersonCam.cs
+++ Assets/Scripts/Core/ThirdPersonCam.cs
@@ -14,32 +14,42 @@ namespace JJBA.Core
         [SerializeField] private Transform playerObj;
         [SerializeField] private Transform combatLookAt;
         [SerializeField] private RagdollSystem ragdollSystem;
+        [SerializeField] private Transform hipsBoneTransform;
+        [SerializeField] private Transform playerTransform;
 
         [Header("Cameras")]
         [SerializeField] private CinemachineFreeLook CombatCamera;
-        [SerializeField] private CinemachineFreeLook DeathCamera;
+        // [SerializeField] private CinemachineFreeLook DeathCamera;
 
 
-        private void Start()
+        private void Awake()
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
             CombatCamera.Priority = 10;
-            DeathCamera.Priority = 0;
+            // DeathCamera.Priority = 0;
+
+            CombatCamera.Follow = playerTransform;
+            CombatCamera.LookAt = combatLookAt;
         }
 
         private void Update()
         {
             if (ragdollSystem != null && ragdollSystem.IsFall())
             {
-                CombatCamera.Priority = 0;
-                DeathCamera.Priority = 10;
+                // CombatCamera.Priority = 0;
+                CombatCamera.Follow = hipsBoneTransform;
+                CombatCamera.LookAt = hipsBoneTransform;
+                // DeathCamera.Priority = 10;
             }
             else
             {
-                CombatCamera.Priority = 10;
-                DeathCamera.Priority = 0;
+                // CombatCamera.Priority = 10;
+                // DeathCamera.Priority = 0;
+
+                CombatCamera.Follow = playerTransform;
+                CombatCamera.LookAt = combatLookAt;
 
                 Vector3 dirToCombatLookAt = combatLookAt.position -
                                         new Vector3(transform.position.x, combatLookAt.position.y,
